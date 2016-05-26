c3ae0c7c1174ec04ee5d4f8545f736518406032a
Nicasio Cascudo <Nicasio Cascudo>
2016-05-26 17:22:54 -0300

    UI api object to separate file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,3 @@
-var api = {
-    orders: {
-        get: function() {
-            return $.get("/api/orders");
-        },
-        put: function(doc, orderNumber) {
-            return $.ajax({
-                        url: "/api/orders/"+orderNumber,
-                        type: 'PUT',
-                        contentType: "application/json",
-                        data: JSON.stringify( doc )
-                    }
-            );
-        },
-        post: function(doc) {
-            return $.ajax({
-                url: "/api/orders/",
-                type: 'POST',
-                contentType: "application/json",
-                data: JSON.stringify( doc )
-            });
-        }
-    },
-    types: {
-        get: function() {
-            return $.get("/api/inspectionTypes/")
-        }
-    }
-} 
-
-
 $(document).ready(function(){
    riot.mount( '*', { 
        events:riot.observable(),
